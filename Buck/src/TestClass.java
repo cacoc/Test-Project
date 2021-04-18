e5bae33e6ab6fcaa2b2de1ff3d96c58b0b3e8181
Banana <Banana@Banana-PC>
2021-04-18 20:51:34 +0800

    Initial commit

---
+++ Buck/src/TestClass.java
@@ -0,0 +1,123 @@
+/*import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestClass {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Account obj[] = new Account[]{ new Account(15, 27), new Account(11, 42) };
+		 */
+		
+//		obj[0] = new Account();
+//		obj[1] = new Account();
+		
+//		obj[0].setData(15, 27);
+//		obj[1].setData(11, 42);
+		
+//		for(Account x : obj) {
+//			System.out.println(x.getA());
+//			System.out.println(x.getB());
+//		}
+		
+		/*
+		 * System.out.println("For array element 0"); obj[0].displayData();
+		 * 
+		 * System.out.println("For array element 1"); obj[1].displayData();
+		 */
+/*		@SuppressWarnings("serial")
+		ArrayList<String> myList = new ArrayList<String>(){{
+			add("One");
+			add("Two");
+			
+		}};
+		System.out.println("Contents in an ArrayList (added anonymously) : " + myList);
+		
+		ArrayList<Integer> myList2 = new ArrayList<Integer>(
+				Arrays.asList(42, 16, 25, 102)
+		);
+		System.out.println("Elements are (Arrays.asList() is used) : " + myList2);
+		
+	
+		String[] array = {"foo", "bar"};
+	    List<String> list = Arrays.asList(array);
+	    array[0] = "baz";*/
+	    
+/*	    
+	    List<Account> accounts = new ArrayList<>();
+	    
+	    accounts.add(new Facebook());
+	    accounts.add(new Facebook());
+	    
+	    // you cannot use a method of a subclass that is not in the superclass
+	    accounts.get(1).post();
+	    
+	    
+	    
+
+		//Account account = new Facebook();
+		
+		
+//		Account obj[] = {
+//				
+//				new Account(1,2),
+//				new Account(3,4)
+//		};
+		
+		
+//		for(Account i: obj) {
+//			System.out.println(i.getA());
+//			System.out.println(i.getB());
+//		}
+	    
+	    /*
+	    
+	    Human h = new Human();
+	    
+	    h.hasName("Jimmy").whoLikes("to eat.");
+	    
+	    System.out.println(h.name + " " + h.like);
+		
+	}
+}
+/*
+class Account {
+	/*
+	 * let say these are the names
+	 * */
+	/*private int a,b;
+	/*
+	public void post() {}/*
+//	public Account(int a, int b) {
+//		this.a = a;
+//		this.b = b;
+//	}
+*/
+/*	
+	public static Account by() {}
+	
+	public Account setData(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public void displayData() {
+		System.out.println("The value of a = " + a);
+		System.out.println("The value of b = " + b);
+	}
+	
+	public int getA() {
+		return a;
+	}
+	
+	public int getB() {
+		return b;
+	}
+}
+
+class Facebook extends Account {
+	public void post() {
+		System.out.println("From facebook class");
+	}
+}
+*/
